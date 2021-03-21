6e2932285733117f158327ebdf54c39329cbfa7b
akiko777 <natsu.hi.boshi731rainbowrose@gmail.com>
2021-03-21 19:25:51 +0900

    READMEに詳細を記載、active storageを導入、バリデーションの記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+  has_many_attached :image
+
+  with_options presence: true do
+    validates :nemu
+    validates :price
+    validates :introduction
+    validates :shop_name
+    validates :category_id
+    validates :eco_appeal
+    validates :images
+  end
+  
 end
